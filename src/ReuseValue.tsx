7963aead42e4aff5326a51fb2cad03415eaf1531
unknown <penpenetai@icloud.com>
2023-08-26 09:32:22 -0700

    changed server url

--- src/ReuseValue.tsx
+++ src/ReuseValue.tsx
@@ -1,7 +1,7 @@
 const renderServer = "https://portfolio-backend-ita5.onrender.com";
 const localhost = "http://localhost:8000";
 const reuseValue = {
-  backendURL: localhost,
+  backendURL: renderServer,
 };
 
 export default reuseValue;
